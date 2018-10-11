ad88bb77deec4d3d61993f92561421b33f034f42
stanley <huangsl@weipaitang.com>
2018-10-11 21:49:40 +0800

    push

--- index.php
+++ index.php
@@ -1,80 +1,221 @@
 <?php
-require_once "./JSSDK/jssdk.php";
-$jssdk = new JSSDK("wx1700f0bbdf2b8f18", "83bb877f35965f13f09a56db69ec4456");
-$signPackage = $jssdk->GetSignPackage();
-?>
-<!DOCTYPE html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;"/>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>微拍堂微信分享案例</title>
-</head>
-<body>
-  <div>
-  微拍堂公众号分享到朋友圈及分享给朋友
-  <p id='info'><?= json_encode($signPackage) ?></p>
-  </div>
-</body>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-<script>
-  window.signPackage =<?= json_encode($signPackage) ?>;
-  /*
-   * 注意：
-   * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
-   * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
-   * 3. 常见问题及完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
-   *
-   * 开发中遇到问题详见文档“附录5-常见错误及解决办法”解决，如仍未能解决可通过以下渠道反馈：
-   * 邮箱地址：weixin-open@qq.com
-   * 邮件主题：【微信JS-SDK反馈】具体问题
-   * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
-   */
-  wx.config({
-    debug: false,
-    appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
-    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-    signature: '<?php echo $signPackage["signature"];?>',
-    jsApiList: [
-      // 所有要调用的 API 都要加到这个列表中
-      'onMenuShareTimeline',
-      'onMenuShareAppMessage'
-    ]
-  });
-  wx.ready(function () {
-    // 分享到朋友圈的实例
-    wx.onMenuShareTimeline({
-    title: '微拍宝成长分享朋友圈', // 分享标题
-    link: 'http://47.100.176.202/content/girl.html', // 分享链接
-    imgUrl: 'http://47.100.176.202/image/b1.jpg', // 分享图标
-    success: function () {
-        // 用户确认分享后执行的回调函数
-        alert("分享朋友圈成功");
-    },
-    cancel: function () { 
-        // 用户取消分享后执行的回调函数
-        alert("分享朋友圈失败");
-    }
-    });
+/**
+  * wechat php test
+  */
 
-    //分享给朋友
-    wx.onMenuShareAppMessage({
-    title: '微拍宝分享给朋友案例', // 分享标题
-    desc: '快来微拍堂做开发工程师 ....', // 分享描述
-    link: 'http://47.100.176.202/content/girl.html', // 分享链接
-    imgUrl: 'http://47.100.176.202/image/b1.jpg', // 分享图标
-    type: '', // 分享类型,music、video或link，不填默认为link
-    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-    success: function () { 
-        // 用户确认分享后执行的回调函数
-        alert("分享friend成功");
-    },
-    cancel: function () { 
-        // 用户取消分享后执行的回调函数
-        alert("分享失败");
+//define your token
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+$flag =$_GET['echostr'];
+
+
+if($flag)
+{
+    $wechatObj->valid();
+}else
+{
+    $wechatObj->responseMsg();
+}
+
+
+
+class wechatCallbackapiTest
+{
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+        	echo $echoStr;
+        	exit;
+        }
     }
-    });
-  });
-</script>
-</html>
+
+    public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+                   the best way is to check the validity of xml by yourself */
+                libxml_disable_entity_loader(true);
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $Event = $postObj->Event;
+                $EventKey = $postObj->EventKey;
+                $MsgType = $postObj->MsgType;
+                $time = time();
+                $textTpl = "<xml>
+    			<ToUserName><![CDATA[%s]]></ToUserName>
+    			<FromUserName><![CDATA[%s]]></FromUserName>
+    			<CreateTime>%s</CreateTime>
+    			<MsgType><![CDATA[%s]]></MsgType>
+    			<Content><![CDATA[%s]]></Content>
+    			<FuncFlag>0</FuncFlag>
+    			</xml>";
+                if($MsgType == "image")
+                {
+                    $MsgType = "text";
+                    $Content = "您发送了一个图片信息";`
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $MsgType, $Content);
+                    echo $resultStr;
+                }
+
+                if($Event == "CLICK" and $EventKey == "V1001_TODAY_MUSIC")
+                {
+                    $MsgType = "text";
+                    $Content = "您点击了今日歌曲";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $MsgType, $Content);
+                    echo $resultStr;
+                }
+
+                $EventKeystr=substr($EventKey,0,8);
+
+                if($Event == "subscribe" && $EventKeystr=="qrscene_")
+                {`
+                    /*
+                        数据库插入
+                        插入  value值=$EventKey=qrscene_生成参数
+                    */
+
+                    $MsgType = "text";
+                    $Content = "您之前未关注平台并且扫描了带参数的二维码";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $MsgType, $Content);
+                    echo $resultStr;
+                }
+
+                if($Event == "subscribe")
+                {
+                    $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[news]]></MsgType>
+                    <ArticleCount>1</ArticleCount>
+                    <Articles>
+                    <item>
+                    <Title><![CDATA[欢迎参加极客学院]]></Title> 
+                    <Description><![CDATA[极客学院微信公众平台开发视频教程]]></Description>
+                    <PicUrl><![CDATA[http://www.sinaimg.cn/dy/slidenews/4_img/2015_11/704_1575962_849639.jpg]]></PicUrl>
+                    <Url><![CDATA[http://www.jikexueyuan.com]]></Url>
+                    </item>
+                    </Articles>
+                    </xml>";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
+                    echo $resultStr;
+                }
+
+                if($Event == "SCAN")
+                {
+                    $MsgType = "text";
+                    $Content = "您之前已关注平台并且扫描了带参数的二维码";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $MsgType, $Content);
+                    echo $resultStr;
+                }
+
+        if(!empty( $keyword ))
+                {
+                if($keyword == "你好")
+                {
+                    $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[news]]></MsgType>
+                    <ArticleCount>1</ArticleCount>
+                    <Articles>
+                    <item>
+                    <Title><![CDATA[您发送了你好]]></Title> 
+                    <Description><![CDATA[极客学院微信公众平台关键字回复]]></Description>
+                    <PicUrl><![CDATA[http://www.sinaimg.cn/dy/slidenews/4_img/2015_11/704_1575962_849639.jpg]]></PicUrl>
+                    <Url><![CDATA[http://www.jikexueyuan.com]]></Url>
+                    </item>
+                    </Articles>
+                    </xml>";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
+                    echo $resultStr;
+
+                }
+                $substr=mb_substr($keyword, 0,2,'utf8');
+                if($substr == "天气")
+                {
+                    $cityname=mb_substr($keyword, 2,5,'utf8');
+                    $urlcityname=urlencode($cityname);
+                    $url="http://v.juhe.cn/weather/index?format=2&cityname=".$urlcityname."&key=29a2c9607b13d34c8998ac646d840728";
+                    $weather=json_decode(gettoken($url),ture);
+
+                    //print_r($weather['result']['today']['dressing_advice']);
+
+                    $wen="当前温度：".$weather['result']['sk']['temp'];//当前温度
+                    $fen="当前风向风级：".$weather['result']['sk']['wind_direction']."-".$weather['result']['sk']['wind_strength'];//当前风向风级
+                    $city="城市：".$weather['result']['today']['city'];//城市
+                    $riqi="日期：".$weather['result']['today']['date_y'];//日期
+                    $wendu="今日温度：".$weather['result']['today']['temperature'];//温度
+                    $chuan="穿衣指数".$weather['result']['today']['dressing_advice'];//穿衣指数
+
+                    $dangweather=$city."\n".$riqi."\n".$wendu."\n".$wen."\n".$fen."\n".$chuan;
+
+                    $MsgType = "text";
+                    $Content = $dangweather;
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $MsgType, $Content);
+                    echo $resultStr;
+                }
+
+
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
+    }
+		
+	private function checkSignature()
+	{
+        // you must define TOKEN by yourself
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+        
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
+
+
+function gettoken($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22");
+    curl_setopt($ch, CURLOPT_ENCODING ,'gzip'); //加入gzip解析
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+?>
